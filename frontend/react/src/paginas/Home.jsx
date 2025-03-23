6caa41d78beeb7edc4e85ce406dbdbdaeea3557d
maximomzts@gmail.com <mmt73@alu.ua.es>
2025-03-23 23:22:01 +0100

    Changed from .js to .jsx

---
+++ frontend/react/src/paginas/Home.jsx
@@ -0,0 +1,20 @@
+
+function Home() {
+return (
+<main className="Home">
+    <section className="about">
+        <p>Hi</p>
+        <p>I'm </p><h1>Máximo Martínez Torres</h1>
+        <p>FullStack Developer and Game Developer</p>
+        <br/>
+        <p>I am a certified Multimedia Engineer from the University of Alicante and a passionate game enthusiast.
+            My expertise in web development primarily focuses on MERN stack projects. Additionally, I have experience in game 
+            development, whether working without an engine using languages like C++ or leveraging frameworks such as Godot Engine.
+        </p>
+    </section>
+</main>
+);
+}
+
+export default Home;
+  
