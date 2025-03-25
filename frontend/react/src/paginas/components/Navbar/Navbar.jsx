97de069a79da674f79985e4318abe62e4a0186dd
maxmzts <maximomzts@gmail.com>
2025-03-25 18:54:57 +0100

    A few tweeks

--- frontend/react/src/paginas/components/Navbar/Navbar.jsx
+++ frontend/react/src/paginas/components/Navbar/Navbar.jsx
@@ -5,12 +5,11 @@ import logo from '../../../assets/logo.svg'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img src={logo} alt="Maximo Martínez Logo" />
+            <img src={logo} alt="Maximo Martínez Logo" aria-hidden="true"/>
             <ul className="nav-menu">
                 <li>Home</li>
                 <li>About Me</li>
                 <li>Portfolio</li>
-                <li>Contact</li>
             </ul>
             <button className="gradient-button">Connect With Me</button>
         </div>
